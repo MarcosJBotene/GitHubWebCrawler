019fdfd48d00bce39514142e0ef867767b7689da
Marcos J. Botene <marcos_j_martins@estudante.sc.senai.br>
2020-11-13 19:58:02 -0200

    Retornando o HTML da pagina de Usuário do GitHub.

--- src/pages/Home/index.php
+++ src/pages/Home/index.php
@@ -1,1 +1,29 @@
 <?php
+
+$proxy = '10.1.21.254:3128';
+
+$arrayConfig = array(
+    'http' => array(
+        'proxy' => $proxy,
+        'request_fulluri' => true
+    ),
+    'https' => array(
+        'proxy' => $proxy,
+        'request_fulluri' => true
+    )
+);
+
+$context = stream_context_create($arrayConfig);
+//-------------------------------------------------------------------------------------------------
+
+$url = 'https://github.com/MarcosJBotene';
+$html = file_get_contents($url, false, $context);
+
+$dom = new DOMDocument();
+libxml_use_internal_errors(true);
+
+// Transforma o HTML em objeto
+$dom->loadHTML($html);
+libxml_clear_errors();
+
+print_r($html);
