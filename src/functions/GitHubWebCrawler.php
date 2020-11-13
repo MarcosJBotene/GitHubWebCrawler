4a081c692df24e7eba272ebb042c98a9753cabdc
Marcos J. Botene <marcos_j_martins@estudante.sc.senai.br>
2020-11-13 20:02:28 -0200

    Adicionado POO.

--- src/functions/GitHubWebCrawler.php
+++ src/functions/GitHubWebCrawler.php
@@ -1,1 +1,46 @@
 <?php
+
+class GitHubWebCrawler
+{
+    private $url;
+    private $proxy;
+    private $dom;
+    private $html;
+
+    public function __construct()
+    {
+        $this->url = "https://github.com/MarcosJBotene";
+        $this->proxy = "10.1.21.254:3128";
+        $this->dom = new DOMDocument();
+    }
+
+    private function getContextConnection()
+    {
+        $arrayConfig = array(
+            'http' => array(
+                'proxy' => $this->proxy,
+                'request_fulluri' => true
+            ),
+            'https' => array(
+                'proxy' => $this->proxy,
+                'request_fulluri' => true
+            )
+        );
+
+        $context = stream_context_create($arrayConfig);
+        return $context;
+    }
+
+    public function loadHTML()
+    {
+        $context = $this->getContextConnection();
+        $this->html = file_get_contents($this->url, false, $context);
+
+        libxml_use_internal_errors(true);
+
+        $this->dom->loadHTML($this->html);
+        libxml_clear_errors();
+
+        print_r($this->html);
+    }
+}
--- src/pages/Home/index.php
+++ src/pages/Home/index.php
@@ -1,29 +1,6 @@
 <?php
 
-$proxy = '10.1.21.254:3128';
+require '../../functions/GitHubWebCrawler.php';
 
-$arrayConfig = array(
-    'http' => array(
-        'proxy' => $proxy,
-        'request_fulluri' => true
-    ),
-    'https' => array(
-        'proxy' => $proxy,
-        'request_fulluri' => true
-    )
-);
-
-$context = stream_context_create($arrayConfig);
-//-------------------------------------------------------------------------------------------------
-
-$url = 'https://github.com/MarcosJBotene';
-$html = file_get_contents($url, false, $context);
-
-$dom = new DOMDocument();
-libxml_use_internal_errors(true);
-
-// Transforma o HTML em objeto
-$dom->loadHTML($html);
-libxml_clear_errors();
-
-print_r($html);
+$test = new GitHubWebCrawler();
+$returningHTML = $test->loadHTML();
